a71b3b803cd67fe306eb19f0c0016501043494f0
rahul4507 <rahulhiragond04@gmail.com>
2025-01-06 00:15:51 +0530

    wrote api to get monthly plan

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,6 +1,8 @@
 const { STATUS } = require("../utils/constants");
 const Tour = require("../models/tourModel");
 const APIFeatures = require("../utils/apiFeatures");
+const { roundFields } = require('../utils/aggregationUtils');
+
 
 exports.aliasTopTours = (req, res, next) => {
   req.query.limit = 5;
@@ -91,39 +93,107 @@ exports.deleteTour = async (req, res) => {
 
 exports.getTourStats = async (req, res) => {
   try {
+    const field = req.query.field
     const stats = await Tour.aggregate([
       {
         $addFields: {
-          ratingsAverageNumeric: { $toDouble: "$ratingsAverage" }
+          ratingsAverageNumeric: { $toDouble: "$ratingsAverage" } // Convert ratingsAverage to number
         }
       },
       {
-        $match: { ratingsAverageNumeric: { $gte: 4.5 } } 
+        $match: { ratingsAverageNumeric: { $gte: 4.5 } } // Filter tours with ratings >= 4.5
       },
       {
         $group: {
-          _id: null,
+          _id: `$${field}`,
+          numTours: { $sum: 1 },
+          numRatings: { $sum: "$ratingsQuantity" },
           avgRating: { $avg: "$ratingsAverageNumeric" },
           avgPrice: { $avg: "$price" },
           minPrice: { $min: "$price" },
           maxPrice: { $max: "$price" }
         }
-      }
+      },
+      {
+        $sort: { minPrice: 1 }
+      },
+      roundFields(["avgRating", "avgPrice", "minPrice", "maxPrice"])
     ]);
 
-    console.log(stats);
     res.status(200).json({
-      status: STATUS.SUCCESS,
+      status: "success",
+      results: stats.length,
       data: {
         stats
       }
     });
-
-
   } catch (err) {
-    res.status(400).json({
-      status: STATUS.FAIL,
-      message: err,
+    res.status(500).json({
+      status: "error",
+      message: err.message
     });
   }
-}
+};
+
+
+exports.getMonthlyPlan = async (req, res) => {
+  try {
+    const year = parseInt(req.params.year, 10);
+    if (isNaN(year)) {
+      return res.status(400).json({
+        status: STATUS.FAIL,
+        message: "Invalid year parameter. Please provide a valid year."
+      });
+    }
+
+    const plan = await Tour.aggregate([
+      {
+        $unwind: "$startDates"
+      },
+      {
+        $match: {
+          startDates: {
+            $gte: new Date(`${year}-01-01`),
+            $lte: new Date(`${year}-12-31`)
+          }
+        }
+      },
+      {
+        $group: {
+          _id: { $month: "$startDates" },
+          numTourStarts: { $sum: 1 },
+          tours: { $push: "$name" }
+        }
+      },
+      {
+        $addFields: {
+          month: '$_id'
+        }
+      },
+      {
+        $project: {
+          _id: 0
+        }
+      },
+      {
+        $sort: { numTourStarts: -1 }
+      },
+      {
+        $limit: 6 
+      }
+    ]);
+
+    res.status(200).json({
+      status: "success",
+      results: plan.length,
+      data: {
+        plan
+      }
+    });
+  } catch (err) {
+    res.status(500).json({
+      status: "error",
+      message: err.message
+    });
+  }
+};
