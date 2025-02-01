94d9eede5781ad5e5a9379448d107d3d958ac4ea
rahul4507 <rahulhiragond04@gmail.com>
2025-02-01 18:42:34 +0530

    added helmet to my application to secure the http and use inbuilt middlewares to secure and also added rate limiting on requests of 100 in one hour

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const helmet = require('helmet');
+const rateLimit = require('express-rate-limit');
 const morgan = require('morgan');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
@@ -13,13 +15,33 @@ if (process.env.NODE_ENV === 'dev') {
   app.use(morgan('dev'));
 }
 
-app.use(express.json());
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP, please try again in an hour!'
+});
 
+
+// 1) GLOBAL MIDDLEWARES
+// Set security HTTP headers
+app.use(helmet());
+
+// Limit requests from same API
+app.use('/api', limiter);
+
+// Body parser, reading data from body into req.body
+app.use(express.json({ limit: '10kb' }));
+
+// Serving static files
+app.use(express.static(`${__dirname}/public`));
+
+// Test middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
 });
 
+// 3) ROUTES  
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
